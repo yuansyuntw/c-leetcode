aeef4c8af02bade140a8c1275433735ce048feb2
yuansyun yeh <yuansyuntw@gmail.com>
2021-09-29 08:36:31 +0800

    [Sorting][Feat] Add 'First Bad Version'

---
+++ sorting and searching/First Bad Version.c
@@ -0,0 +1,28 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+
+int checkBadVersion(int startVersion, int endVersion) {
+    if ((endVersion - startVersion) == 0) {
+        if (isBadVersion(startVersion)) {
+            return startVersion;
+        } else {
+            return (long)(startVersion + 1);
+        }
+    } else if ((endVersion - startVersion) < 0) {
+        return startVersion;
+    }
+    
+    int middleVersion = ((long)startVersion + (long)endVersion)/2;
+    if (!isBadVersion(middleVersion)) {
+        startVersion = middleVersion + 1;
+        return checkBadVersion(startVersion, endVersion);
+    } else {
+        endVersion = middleVersion - 1;
+        return checkBadVersion(startVersion, endVersion);
+    }
+}
+
+int firstBadVersion(int n) {
+    return checkBadVersion(1, n);
+}
