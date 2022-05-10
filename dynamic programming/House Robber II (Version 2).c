3f56d5e381fb4e61e63e43ff810e8023fd2eddd7
yuansyun yeh <yuansyuntw@gmail.com>
2022-05-10 20:44:10 +0800

    [DP][Feat] Add 'House Robber II (Version 2)'

---
+++ dynamic programming/House Robber II (Version 2).c
@@ -0,0 +1,70 @@
+int dpSize = 4;
+int aheadThreeCellIndex = 0;
+int aheadTwoCellIndex = 1;
+int aheadOneCellIndex = 2;
+int currentCellIndex = 3;
+
+void setCurrentCellMoney(int* dp, int currentCellMoney) {
+    int moneyAheadTwoCell = currentCellMoney + dp[aheadTwoCellIndex];
+    int moneyAheadThreeCell = currentCellMoney + dp[aheadThreeCellIndex];
+
+    int result;
+    if (moneyAheadTwoCell > moneyAheadThreeCell) {
+        result = moneyAheadTwoCell;
+    } else {
+        result = moneyAheadThreeCell;
+    }
+    
+    dp[currentCellIndex] = result;
+}
+
+int rob(int* nums, int numsSize){
+    int max = nums[0];
+    
+    for (int i=0;((i<dpSize)&&(i<numsSize));i++) {
+        if (nums[i] > max) max = nums[i];
+    }
+    
+    if (numsSize > currentCellIndex) {
+        int money = nums[aheadThreeCellIndex] + nums[aheadOneCellIndex];
+        if (money > max) max = money;
+    }
+    
+    if (numsSize < dpSize) return max;
+    
+    int* dp = calloc(sizeof(int), dpSize);          
+    int* lastOneDP = calloc(sizeof(int), dpSize);
+    
+    dp[aheadThreeCellIndex] = nums[aheadThreeCellIndex];
+    dp[aheadTwoCellIndex] = nums[aheadTwoCellIndex];
+    dp[aheadOneCellIndex] = nums[aheadOneCellIndex] + dp[aheadThreeCellIndex];
+                     
+    lastOneDP[aheadThreeCellIndex] = 0; // the last one dp can't take the first house.
+    lastOneDP[aheadTwoCellIndex] = nums[aheadTwoCellIndex];
+    lastOneDP[aheadOneCellIndex] = nums[aheadOneCellIndex] + lastOneDP[aheadThreeCellIndex];
+
+    for (int i=currentCellIndex;i<numsSize;i++) {
+        setCurrentCellMoney(dp, nums[i]);
+        setCurrentCellMoney(lastOneDP, nums[i]);
+        
+        int lastOneIndex = numsSize-1;
+        if ((i < lastOneIndex) && (dp[currentCellIndex] > max)) {
+            max = dp[currentCellIndex];
+        }
+        
+        if ((i == lastOneIndex) && (lastOneDP[currentCellIndex] > max)) {
+            max = lastOneDP[currentCellIndex];
+        }
+        
+        // shift
+        for (int c=0;c<dpSize-1;c++) {
+            dp[c] = dp[c+1];
+            lastOneDP[c] = lastOneDP[c+1];
+        }
+    }
+    
+    free(dp);
+    free(lastOneDP);
+    
+    return max;
+}
