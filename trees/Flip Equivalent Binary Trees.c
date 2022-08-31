fb7b2a252c164cc2dafe5f580ce106e49b4ec909
yuansyun yeh <yuansyuntw@gmail.com>
2022-08-31 21:07:58 +0800

    Update the '951 Flip Equivalent Binary Trees'

--- trees/Flip Equivalent Binary Trees.c
+++ trees/Flip Equivalent Binary Trees.c
@@ -7,20 +7,6 @@
  * };
  */
 
-bool isEqual(struct TreeNode* node1, struct TreeNode* node2) {
-    if ((node1 == NULL) || (node2 == NULL)) {
-        bool areEmpty = (node1 == node2);
-        if (areEmpty) {
-            return true;
-        }
-        
-        return false;
-    }
-    
-    bool areSame = (node1->val == node2->val);
-    return areSame;
-}
-
 bool flipEquiv(struct TreeNode* root1, struct TreeNode* root2){
     if ((root1 == NULL) || (root2 == NULL)) {
         if (root1 == root2) {
@@ -36,30 +22,12 @@ bool flipEquiv(struct TreeNode* root1, struct TreeNode* root2){
     }
     
     // no-flip
-    bool isSameChilds = (isEqual(root1->left, root2->left) && isEqual(root1->right, root2->right));
+    bool isSameChilds = flipEquiv(root1->left, root2->left) && flipEquiv(root1->right, root2->right);
     if (isSameChilds) {
-        bool isLeftEquivalent = flipEquiv(root1->left, root2->left);
-        bool isRightEquivalent = flipEquiv(root1->right, root2->right);
-        if (isLeftEquivalent && isRightEquivalent) {
-            return true;
-        }
-    }
-    
-    // flip
-    bool isCanFlip = (isEqual(root1->left, root2->right) && isEqual(root1->right, root2->left));
-    if (isCanFlip) {
-         bool isLeftEquivalent = flipEquiv(root1->left, root2->right);
-        if (!isLeftEquivalent) {
-            return false;
-        }
-        
-        bool isRightEquivalent = flipEquiv(root1->right, root2->left);
-        if (!isRightEquivalent) {
-            return false;
-        }
-        
         return true;
     }
     
-    return false;
+    // flip
+    bool isSameFlipedChilds = flipEquiv(root1->left, root2->right) && flipEquiv(root1->right, root2->left);
+    return isSameFlipedChilds;
 }
