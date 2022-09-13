a49e191ef919d90f269f308474f0cc54e4b713b8
yuansyun yeh <yuansyuntw@gmail.com>
2022-09-13 21:16:03 +0800

    Add comments

--- array/Trapping Rain Water.c
+++ array/Trapping Rain Water.c
@@ -14,12 +14,16 @@ int trap(int* height, int heightSize) {
     int l = 0;
     int r = heightSize - 1;
 
+    // this method put two processing together:
     while (l < r) {
+        // 1. pick the smaller height
         if (maxLeft < maxRight) {
+            // check whether is a trap
             if ((maxLeft - height[l]) > 0) {
                 result += (maxLeft - height[l]);
             }
 
+            // 2. forward the max left value
             l++;
             maxLeft = max(maxLeft, height[l]);
         }
