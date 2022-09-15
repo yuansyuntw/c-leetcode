e07832c6bbae50e7a4859310f89186396394d563
yuansyun yeh <yuansyuntw@gmail.com>
2022-09-15 21:29:40 +0800

    Format the file

--- hashtable/Hand of Straights.c
+++ hashtable/Hand of Straights.c
@@ -1,89 +1,108 @@
-struct Item {
+struct Item
+{
     UT_hash_handle hh;
     int key;
     int value;
 };
 
-int sortByKey(struct Item* itemA, struct Item* itemB) {
-    if (itemA->key > itemB->key) {
+int sortByKey(struct Item *itemA, struct Item *itemB)
+{
+    if (itemA->key > itemB->key)
+    {
         return 1;
-    } else if (itemA->key < itemB->key) {
+    }
+    else if (itemA->key < itemB->key)
+    {
         return -1;
-    } else {
+    }
+    else
+    {
         return 0;
     }
 }
 
-bool isNStraightHand(int* hand, int handSize, int groupSize) {
-	bool isPossible = ((handSize % groupSize) == 0);
-	if (!isPossible) {
-		return false;
-	}
+bool isNStraightHand(int *hand, int handSize, int groupSize)
+{
+    bool isPossible = ((handSize % groupSize) == 0);
+    if (!isPossible)
+    {
+        return false;
+    }
 
     struct Item *hashTable = NULL;
 
-	for (int i = 0; i < handSize; i++) {
+    for (int i = 0; i < handSize; i++)
+    {
         int key = hand[i];
         struct Item *item = NULL;
-        
+
         HASH_FIND_INT(hashTable, &key, item);
         bool isNotFound = (item == NULL);
-        if (isNotFound) {
+        if (isNotFound)
+        {
             item = calloc(1, sizeof(struct Item));
             item->key = hand[i];
             item->value = 0;
-        } else {
+        }
+        else
+        {
             HASH_DEL(hashTable, item);
         }
-        
+
         item->value = item->value + 1;
         HASH_ADD_INT(hashTable, key, item);
-	}
-    
+    }
+
     HASH_SORT(hashTable, sortByKey);
     int groupCount = 0;
-    struct Item* item = hashTable;
+    struct Item *item = hashTable;
     int lastKey = item->key - 1;
-    while(item != NULL) {
-        struct Item* nextItem = item->hh.next;
-        
+    while (item != NULL)
+    {
+        struct Item *nextItem = item->hh.next;
+
         bool isCanTake = ((item->key - lastKey) == 1);
-        if (!isCanTake) {
+        if (!isCanTake)
+        {
             groupCount = 1;
             break;
         }
-        
+
         lastKey = item->key;
         groupCount++;
         item->value--;
-        if (item->value == 0) {
+        if (item->value == 0)
+        {
             // Pop the wrong item, it make the next group is not consecutive.
             bool isStop = (item != hashTable);
             HASH_DEL(hashTable, item);
             free(item);
-            if (isStop) {
+            if (isStop)
+            {
                 groupCount = 1;
                 break;
             }
         }
-        
-        if (groupCount == groupSize) {
+
+        if (groupCount == groupSize)
+        {
             groupCount = 0;
             item = hashTable;
-            
+
             bool isStop = (item == NULL);
-            if (isStop) {
+            if (isStop)
+            {
                 break;
             }
-            
+
             lastKey = item->key - 1;
             continue;
         }
-        
+
         item = nextItem;
     }
-    
+
     HASH_CLEAR(hh, hashTable);
 
-	return (groupCount == 0);
+    return (groupCount == 0);
 }
