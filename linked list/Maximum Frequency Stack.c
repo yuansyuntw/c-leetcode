36b475ec01998ee3b5fa1df3e31565f1a5a1b812
yuansyun yeh <yuansyuntw@gmail.com>
2022-10-18 22:39:03 +0800

    Format the code

--- linked list/Maximum Frequency Stack.c
+++ linked list/Maximum Frequency Stack.c
@@ -1,73 +1,85 @@
 const int COUNT_TO_LINKLISTITEMS_MAP_LENGTH = 20000;
-typedef struct LinkListItem {
-	int value;
-	struct LinkListItem* nextItem;
+typedef struct LinkListItem
+{
+    int value;
+    struct LinkListItem *nextItem;
 } LinkListItem;
 
-typedef struct {
+typedef struct
+{
     int value;
-    int frequency;	
+    int frequency;
     UT_hash_handle hh;
 } HashTableItem;
 
-typedef struct {
+typedef struct
+{
     int maxFrequency;
-    HashTableItem* hashtable;
-    LinkListItem** countToItemsMap;
+    HashTableItem *hashtable;
+    LinkListItem **countToItemsMap;
 } FreqStack;
 
-FreqStack* freqStackCreate() {
-   	FreqStack *stack = (FreqStack *)calloc(1, sizeof *stack);
-	stack->maxFrequency = 0;
+FreqStack *freqStackCreate()
+{
+    FreqStack *stack = (FreqStack *)calloc(1, sizeof *stack);
+    stack->maxFrequency = 0;
     stack->hashtable = NULL;
-    stack->countToItemsMap = (LinkListItem**)calloc(COUNT_TO_LINKLISTITEMS_MAP_LENGTH , sizeof(LinkListItem*));
+    stack->countToItemsMap = (LinkListItem **)calloc(COUNT_TO_LINKLISTITEMS_MAP_LENGTH, sizeof(LinkListItem *));
     return stack;
 }
 
-HashTableItem* updateHashTable(FreqStack* obj, int value) {
-    HashTableItem* item = NULL;
+HashTableItem *updateHashTable(FreqStack *obj, int value)
+{
+    HashTableItem *item = NULL;
 
     HASH_FIND_INT(obj->hashtable, &value, item);
     bool isNotFind = (item == NULL);
-    if (isNotFind) {
+    if (isNotFind)
+    {
         item = (HashTableItem *)calloc(1, sizeof *item);
         item->value = value;
         item->frequency = 0;
     }
 
     item->frequency++;
-    HashTableItem* oldItem;
-	HASH_REPLACE_INT(obj->hashtable, value, item, oldItem);
-	
-	return item;
+    HashTableItem *oldItem;
+    HASH_REPLACE_INT(obj->hashtable, value, item, oldItem);
+
+    return item;
 }
 
-void updateCountToItemsLinkList(FreqStack* obj, HashTableItem* item) {
-	int currentFrequency = item->frequency;
-	LinkListItem *lastItem = obj->countToItemsMap[currentFrequency];
-	LinkListItem *currentItem = lastItem;
-	while(currentItem != NULL) {
-		lastItem = currentItem;
-		currentItem = currentItem->nextItem;
+void updateCountToItemsLinkList(FreqStack *obj, HashTableItem *item)
+{
+    int currentFrequency = item->frequency;
+    LinkListItem *lastItem = obj->countToItemsMap[currentFrequency];
+    LinkListItem *currentItem = lastItem;
+    while (currentItem != NULL)
+    {
+        lastItem = currentItem;
+        currentItem = currentItem->nextItem;
     }
-    
 
-    LinkListItem* newItem = (LinkListItem *)calloc(1, sizeof(LinkListItem));
+    LinkListItem *newItem = (LinkListItem *)calloc(1, sizeof(LinkListItem));
     newItem->value = item->value;
 
     bool isEmpty = (lastItem == NULL);
-    if (isEmpty) {
+    if (isEmpty)
+    {
         obj->countToItemsMap[currentFrequency] = newItem;
-    } else {
+    }
+    else
+    {
         lastItem->nextItem = newItem;
     }
 }
 
-void freqStackPush(FreqStack* obj, int val) {
-	HashTableItem* item = updateHashTable(obj, val);
+void freqStackPush(FreqStack *obj, int val)
+{
+    HashTableItem *item = updateHashTable(obj, val);
 
-    bool isUpdatedMaxFrequency = (item->frequency > obj->maxFrequency);	
-    if (isUpdatedMaxFrequency) {
+    bool isUpdatedMaxFrequency = (item->frequency > obj->maxFrequency);
+    if (isUpdatedMaxFrequency)
+    {
         obj->maxFrequency = item->frequency;
     }
 
@@ -75,24 +87,28 @@ void freqStackPush(FreqStack* obj, int val) {
 }
 
 const int INVALID_VALUE = -1;
-int removeLastItemForTargetCount(FreqStack* obj) {
-	LinkListItem* start = obj->countToItemsMap[obj->maxFrequency];
+int removeLastItemForTargetCount(FreqStack *obj)
+{
+    LinkListItem *start = obj->countToItemsMap[obj->maxFrequency];
     bool isEmpty = (start == NULL);
-    if (isEmpty) {
+    if (isEmpty)
+    {
         return INVALID_VALUE;
     }
-    
-    LinkListItem* previous = start;
-    LinkListItem* last = start->nextItem;
-    if (last == NULL) {
+
+    LinkListItem *previous = start;
+    LinkListItem *last = start->nextItem;
+    if (last == NULL)
+    {
         int value = start->value;
         free(start);
         obj->countToItemsMap[obj->maxFrequency] = NULL;
         obj->maxFrequency--;
         return value;
     }
-    
-    while(last->nextItem != NULL) {
+
+    while (last->nextItem != NULL)
+    {
         previous = last;
         last = last->nextItem;
     }
@@ -103,38 +119,46 @@ int removeLastItemForTargetCount(FreqStack* obj) {
     return value;
 }
 
-void decendingItemFrequency(FreqStack* obj, int targetValue) {
-	HashTableItem* item;
+void decendingItemFrequency(FreqStack *obj, int targetValue)
+{
+    HashTableItem *item;
 
-	HASH_FIND_INT(obj->hashtable, &targetValue, item);
-	if (item != NULL) {
-		item->frequency--;
-        
+    HASH_FIND_INT(obj->hashtable, &targetValue, item);
+    if (item != NULL)
+    {
+        item->frequency--;
+
         HashTableItem *oldItem;
-		HASH_REPLACE_INT(obj->hashtable, value, item, oldItem);
+        HASH_REPLACE_INT(obj->hashtable, value, item, oldItem);
     }
 }
 
-int freqStackPop(FreqStack* obj) {
-	int value = removeLastItemForTargetCount(obj);
-	if (value == INVALID_VALUE) {
-		return INVALID_VALUE;
+int freqStackPop(FreqStack *obj)
+{
+    int value = removeLastItemForTargetCount(obj);
+    if (value == INVALID_VALUE)
+    {
+        return INVALID_VALUE;
     }
 
-	decendingItemFrequency(obj, value);
-	return value;
+    decendingItemFrequency(obj, value);
+    return value;
 }
 
-void freqStackFree(FreqStack* obj) {
+void freqStackFree(FreqStack *obj)
+{
     HashTableItem *currentItem, *tmp;
 
-	HASH_ITER(hh, obj->hashtable, currentItem, tmp) {
-		free(currentItem);
+    HASH_ITER(hh, obj->hashtable, currentItem, tmp)
+    {
+        free(currentItem);
     }
 
-    for (int i=1;i<obj->maxFrequency; i++) {
+    for (int i = 1; i < obj->maxFrequency; i++)
+    {
         LinkListItem *item = obj->countToItemsMap[i];
-        while(item != NULL) {
+        while (item != NULL)
+        {
             LinkListItem *tmp = item->nextItem;
             free(item);
             item = tmp;
@@ -144,13 +168,12 @@ void freqStackFree(FreqStack* obj) {
     free(obj);
 }
 
-
 /**
  * Your FreqStack struct will be instantiated and called as such:
  * FreqStack* obj = freqStackCreate();
  * freqStackPush(obj, val);
- 
+
  * int param_2 = freqStackPop(obj);
- 
+
  * freqStackFree(obj);
 */
